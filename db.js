4e5c3efb9d25ba017defc5b1b47454c384043f39
pujaKundu <iampujakundu@gmail.com>
2022-09-07 03:14:28 +0600

    'transac'

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 
 const videos = require("./data/db_videos");
 const tags = require("./data/db_tags");
+const transactions = require(".data/db_transactionList");
 
 module.exports = {
  
   videos,
-tags
+tags,
+transactions
  
 }
 
